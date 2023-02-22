d42e7d9fab72a132923afa83727677f508604bb5
renoxvi <vicreno08@gmail.com>
2023-02-22 11:02:54 -0800

    Prototypes

--- sort.h
+++ sort.h
@@ -16,7 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void swap(int *arr, int idx1, int idx2);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
@@ -25,4 +24,12 @@ void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
