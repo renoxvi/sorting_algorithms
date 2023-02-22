94d7f42d90e48f696d733f6dc6fce4ebfaab460a
renoxvi <vicreno08@gmail.com>
2023-02-22 11:42:41 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,29 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array lements from min to max value
+ * bubble_sort - sort array elements from min to max value
  * @array: array
  * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
+    size_t i, index;
+    int tmp;
 
-	size_t i, index, tmp = 0;
+    if (size < 2)
+        return;
 
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
-			}
-		}
+    for (i = 0; i < size - 1; i++)
+    {
+        for (index = 0; index < size - i - 1; index++)
+        {
+            if (array[index] > array[index + 1])
+            {
+                tmp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
 }
+
