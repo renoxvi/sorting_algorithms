a44d0ddfe89bd3fbfbfb374306477b3299ffe841
renoxvi <vicreno08@gmail.com>
2023-02-22 11:51:17 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,24 +6,24 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, index;
-    int tmp;
+	size_t i, index;
+	int tmp;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (index = 0; index < size - i - 1; index++)
-        {
-            if (array[index] > array[index + 1])
-            {
-                tmp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (index = 0; index < size - i - 1; index++)
+		{
+			if (array[index] > array[index + 1])
+			{
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
 
